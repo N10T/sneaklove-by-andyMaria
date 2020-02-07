dee52252a017ea20362c1788e931c46108df8e1a
N10T <andy.tisba@gmail.com>
2020-02-07 12:33:44 +0100

    update ok with tags

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -6,43 +6,50 @@ const protectRoute = require("./../middlewares/protectRoute");
 
 // CREATE PRODUCTS & TAGS
 
-router.get("/prod-add", protectRoute,(req, res) => {
-    tagModel
-    .find()
-    .then(dbRes => {
-        const tags = dbRes;
-        res.render("products_add", { tags });
-    })
+router.get("/prod-add", protectRoute, (req, res) => {
+  tagModel.find().then(dbRes => {
+    const tags = dbRes;
+    res.render("products_add", { tags });
+  });
 });
 
 router.post("/prod-add", protectRoute, (req, res, next) => {
-    const { name, ref, sizes, price, image, description, category, id_tags} = req.body;
-    sneakerModel
+  const {
+    name,
+    ref,
+    sizes,
+    price,
+    image,
+    description,
+    category,
+    id_tags
+  } = req.body;
+  sneakerModel
     .create({
-        name,
-        ref,
-        sizes,
-        price,
-        image,
-        description,
-        category,
-        id_tags
+      name,
+      ref,
+      sizes,
+      price,
+      image,
+      description,
+      category,
+      id_tags
     })
     .then(dbRes => {
-        // req.flash("success", "product successfully created");
-        res.redirect("/prod-add");
+      // req.flash("success", "product successfully created");
+      res.redirect("/prod-add");
     })
     .catch(next);
 });
 
 router.post("/tag-add", protectRoute, (req, res, next) => {
-    const {label} = req.body;
-    tagModel
+  const { label } = req.body;
+  tagModel
     .create({
-        label
+      label
     })
     .then(() => {
-        res.redirect("/prod-add");
+      res.redirect("/prod-add");
     })
     .catch(next);
 });
@@ -50,152 +57,92 @@ router.post("/tag-add", protectRoute, (req, res, next) => {
 // MANAGE PRODUCTS
 
 router.get("/prod-manage", protectRoute, (req, res, next) => {
-    sneakerModel
+  sneakerModel
     .find()
     .then(sneakers => {
       res.render("products_manage", { sneakers });
     })
     .catch(dbErr => {
-      console.log(`Oh no... a problem happend with the database and the products_manage page`);
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      console.log(
+        `Oh no... a problem happend with the database and the products_manage page`
+      );
+    });
+});
 
 // DELETE PRODUCT
 router.get("/product-delete/:id", protectRoute, (req, res, next) => {
-    sneakerModel
+  sneakerModel
     .findByIdAndDelete(req.params.id)
     .then(sneaker => {
-        console.log(sneaker.name, " delete")
-        res.redirect("/prod-manage");
+      console.log(sneaker.name, " delete");
+      res.redirect("/prod-manage");
     })
     .catch(dbErr => {
-      console.log(`Oh no... a problem happend with the database and the delete button`);
-    })
-})
+      console.log(
+        `Oh no... a problem happend with the database and the delete button`
+      );
+    });
+});
 
 // UPDATE PRODUCT
 router.get("/product-edit/:id", protectRoute, (req, res, next) => {
-    sneakerModel
-    .findById(req.params.id)
-    .then(sneaker => {
-        console.log(sneaker.name, " go update page")
-        res.render("product_edit",{sneaker});
+  Promise.all([
+    tagModel.find(),
+    sneakerModel.findById(req.params.id).populate("id_tags")
+  ])
+    .then(dbRes => {
+      console.log(dbRes, " go update page");
+      res.render("product_edit", { sneaker: dbRes[1], tags: dbRes[0] });
     })
     .catch(dbErr => {
-      console.log(`Oh no... a problem happend with the database and the update of ${sneaker.name}`);
-    })
-})
+      console.log(
+        `Oh no... a problem happend with the database and the update of ${dbErr}`
+      );
+    });
+});
 
 router.post("/product-edit/:id", protectRoute, (req, res, next) => {
-    const {
-        name,
-        ref,
-        sizes,
-        price,
-        image,
-        description,
-        category,
-        id_tags
-    } = req.body  
-    
-    sneakerModel
-    .findByIdAndUpdate(req.params.id,{
-        name,
-        ref,
-        sizes,
-        price,
-        image,
-        description,
-        category,
-        id_tags
+  const {
+    name,
+    ref,
+    sizes,
+    price,
+    image,
+    description,
+    category,
+    id_tags
+  } = req.body;
+  console.log({
+    name,
+    ref,
+    sizes,
+    price,
+    image,
+    description,
+    category,
+    id_tags
+  });
+
+  sneakerModel
+    .findByIdAndUpdate(req.params.id, {
+      name,
+      ref,
+      sizes,
+      price,
+      image,
+      description,
+      category,
+      id_tags
     })
     .then(sneaker => {
-        console.log(sneaker.name, " have been updated")
-        res.redirect("/prod-manage");
+      console.log(sneaker.name, " have been updated");
+      res.redirect("/prod-manage");
     })
     .catch(dbErr => {
-      console.log(`Oh no... a problem happend with the database and the update of ${sneaker.name}`);
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      console.log(
+        `Oh no... a problem happend with the database and the update of ${dbErr}`
+      );
+    });
+});
 
 module.exports = router;
