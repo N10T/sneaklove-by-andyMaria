d22b833f3d1bdcd0b85b59131801e016ad9171a5
ina <mgmuniza@gmail.com>
2020-02-07 10:14:43 +0100

    prod-add work in progress

---
+++ middlewares/exposeFlashMessage.js
@@ -0,0 +1,5 @@
+module.exports = function ©(req, res, next) {
+    res.locals.success_msg = req.flash("success");
+    res.locals.error_msg = req.flash("error");
+    next();
+  };
--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -3,14 +3,30 @@ const router = new express.Router(); // create an app sub-module (router)
 const sneakerModel = require("./../models/Sneaker");
 const protectRoute = require("./../middlewares/protectRoute");
 
-// CREATE PRODUCTS
+// CREATE PRODUCTS & TAGS
 
 router.get("/prod-add", protectRoute,(req, res) => {
     res.render("products_add");
 });
 
-router.post("/prod-add", protectRoute, (req, res next) => {
-    
-})
+router.post("/prod-add", protectRoute, (req, res, next) => {
+    const { name, ref, sizes, price, image, description, category, id_tags} = req.body;
+    sneakerModel
+    .create({
+        name,
+        ref,
+        sizes,
+        price,
+        image,
+        description,
+        category,
+        id_tags
+    })
+    .then(dbRes => {
+        // req.flash("success", "product successfully created");
+        res.redirect("/prod-add");
+    })
+    .catch(next);
+});
 
 module.exports = router;
